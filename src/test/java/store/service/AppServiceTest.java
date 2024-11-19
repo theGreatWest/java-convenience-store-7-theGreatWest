671c538fbc022387f8b0541782a3a80823a12445
youngjuKim <kjuzoojuk@naver.com>
2024-11-20 06:54:57 +0900

    test: appService 재고 상황 안내 테스트 코드 추가

---
+++ src/test/java/store/service/AppServiceTest.java
@@ -0,0 +1,36 @@
+package store.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppServiceTest {
+    private AppService appService = new AppService(new DatabaseService(), new ValidationService());
+
+    @DisplayName("재고 상황을 정확히 안내할 수 있다.")
+    @Test
+    void 재고_상황_정확히_안내() {
+        assertThat(appService.stockStatus()).contains(
+                "- 콜라 1,000원 10개 탄산2+1",
+                "- 콜라 1,000원 10개",
+                "- 사이다 1,000원 8개 탄산2+1",
+                "- 사이다 1,000원 7개",
+                "- 오렌지주스 1,800원 9개 MD추천상품",
+                "- 오렌지주스 1,800원 재고 없음",
+                "- 탄산수 1,200원 5개 탄산2+1",
+                "- 탄산수 1,200원 재고 없음",
+                "- 물 500원 10개",
+                "- 비타민워터 1,500원 6개",
+                "- 감자칩 1,500원 5개 반짝할인",
+                "- 감자칩 1,500원 5개",
+                "- 초코바 1,200원 5개 MD추천상품",
+                "- 초코바 1,200원 5개",
+                "- 에너지바 2,000원 5개",
+                "- 정식도시락 6,400원 8개",
+                "- 컵라면 1,700원 1개 MD추천상품",
+                "- 컵라면 1,700원 10개"
+        );
+    }
+
+}
