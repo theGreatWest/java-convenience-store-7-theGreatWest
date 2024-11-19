13111dbbc009d1e9f827de8f946e67c19040ca4b
youngjuKim <kjuzoojuk@naver.com>
2024-11-20 06:58:15 +0900

    feat: 사용자 출력 관련 코드 추가

---
+++ src/main/java/store/view/OutputView.java
@@ -0,0 +1,10 @@
+package store.view;
+
+import store.core.constant.Constant;
+
+public class OutputView {
+    public void printWelcomeStockStatus(String stockStatus){
+        System.out.println(Constant.WELCOME_MESSAGE + stockStatus + Constant.INPUT_PRODUCTS_NAME_QUANTITY);
+    }
+
+}
