6de1c6e369ef2d3bf8672a59a7b1eebbedfdac26
youngjuKim <kjuzoojuk@naver.com>
2024-11-20 06:59:08 +0900

    feat: appConfig 파일 이용해 환경 세팅

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,12 +1,12 @@
 package store;
 
-import store.controller.ApplicationController;
-import store.core.config.ApplicationConfig;
+import store.controller.AppController;
+import store.core.config.AppConfig;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationConfig appConfig = new ApplicationConfig();
-        ApplicationController appController = appConfig.appController();
+        AppConfig appConfig = new AppConfig();
+        AppController appController = appConfig.appController();
 
         appController.run();
     }
